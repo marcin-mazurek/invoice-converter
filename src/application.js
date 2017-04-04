331b3c795cb5d0f762fbe62f1873835ecc19187d
Marcin Mazurek <marcin@mazurek.pro>
2017-04-04 21:10:01 +0200

    Clean up

--- src/application.js
+++ src/application.js
@@ -1,9 +1,5 @@
 const initializeDragAndDrop = require('./view/drag-and-drop');
 const preventPageChangeOnFileDrop = require('./view/prevent-page-change-on-file-drop');
 
-function runApplication() {
-  preventPageChangeOnFileDrop();
-  initializeDragAndDrop(document.getElementById('dragAndDropArea'));
-}
-
-runApplication();
+preventPageChangeOnFileDrop();
+initializeDragAndDrop(document.getElementById('dragAndDropArea'));
