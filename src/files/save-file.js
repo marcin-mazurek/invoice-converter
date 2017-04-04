e030aae86215aaa1458b198767520de1141d7549
Marcin Mazurek <marcin@mazurek.pro>
2017-04-04 23:12:03 +0200

    Stop opening dev tools by default

--- src/files/save-file.js
+++ src/files/save-file.js
@@ -1,25 +1,28 @@
 const app = require('electron').remote;
 const fs = require('fs');
 
+function generateFileName() {
+  const date = new Date().toLocaleDateString('en-GB', {
+    day: 'numeric',
+    month: 'numeric',
+    year: 'numeric',
+    hour: 'numeric',
+    minute: 'numeric',
+    second: 'numeric'
+  })
+  .split('/')
+  .join('-')
+  .split(', ')
+  .join('-')
+  .split(':')
+  .join('-');
+
+  return `faktura-${date}.xml`;
+}
+
 module.exports = function (content) {
   return new Promise((resolve, reject) => {
-    const date = new Date().toLocaleDateString('en-GB', {
-      day: 'numeric',
-      month: 'numeric',
-      year: 'numeric',
-      hour: 'numeric',
-      minute: 'numeric',
-      second: 'numeric'
-    })
-    .split('/')
-    .join('-')
-    .split(', ')
-    .join('-')
-    .split(':')
-    .join('-');
-    const defaultPath = `faktura-${date}.xml`;
-
-    app.dialog.showSaveDialog({ defaultPath }, fileName => {
+    app.dialog.showSaveDialog({ defaultPath: generateFileName() }, fileName => {
       if (fileName === undefined) {
         resolve();
         return;
